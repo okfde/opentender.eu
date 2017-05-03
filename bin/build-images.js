35397db0e556afb07548acac97c09e615f785c7c
ffalt <ffalty@gmail.com>
2017-05-03 13:18:45 +0200

    fixed download image filenames for sub-tables

--- bin/build-images.js
+++ bin/build-images.js
@@ -22,9 +22,7 @@ const toPng = function (table) {
 		var c = countries_ids.filter(function (c) {
 			return e.id == c.name;
 		})[0];
-		// console.log(c.iso);
 		css.push('#' + c.iso + ' {fill:' + scale(e.group).hex() + ';}');
-		// console.log();
 	});
 	const svgbuffer = new Buffer(svgslice[0] + css.join(' ') + svgslice[1]);
 	const pngbuffer = svg2png.sync(svgbuffer, {width: 783 * size, height: 709 * size});
@@ -36,30 +34,9 @@ tables.forEach(function (t) {
 	if (t.entries) toPng(t);
 	else {
 		t.values.forEach(function (t2, i) {
-			t2.nr = t.nr + '.' + i;
+			t2.nr = t.nr + '.' + (i + 1);
 			t2.groups = t2.groups || t.groups;
 			toPng(t2);
 		})
 	}
 });
-// drawtables.forEach(function (table) {
-// 	countries_ids.forEach(function (c) {
-// 		if (c.tables && c.tables[table.nr]) {
-// 			var val =
-// 				scale(entry.group).hex();
-// 			console.log(c.tables[table.nr]);
-//
-// 		}
-// 	});
-// });
-
-
-// const svg = fs.readFileSync("../web/mapping/assets/svg/borders_iso.svg").toString();
-// const svgbuffer = new Buffer(svg);
-// const pngbuffer = svg2png.sync(svgbuffer, { width: 3000, height: 3000 });
-// fs.writeFileSync("dest.png", pngbuffer);
-
-// fs.readFile("../web/mapping/assets/borders_iso.svg")
-//     .then(sourceBuffer => svg2png(sourceBuffer, { width: 3000, height: 3000 }))
-//     .then(buffer => fs.writeFile("dest.png", buffer))
-//     .catch(e => console.error(e));
